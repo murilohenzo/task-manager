6f518718d695881543ed313f0482bbafbb521d85
Rhamanda Lima <cathyannarhamanda@gmail.com>
2024-05-29 16:51:19 -0300

    feat: create dashboard module

---
+++ task-manager-front-end/src/app/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+@NgModule({
+  declarations: [DashboardComponent],
+  imports: [CommonModule, SharedModule]
+})
+export class DashboardModule {}
