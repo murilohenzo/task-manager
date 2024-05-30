874f6d6f5e32aa81c1fcf0e8557550808cafd203
Rhamanda Lima <cathyannarhamanda@gmail.com>
2024-05-30 12:10:02 -0300

    feat: create delete task modal component

--- task-manager-front-end/src/app/modules/dashboard/dashboard.module.ts
+++ task-manager-front-end/src/app/modules/dashboard/dashboard.module.ts
@@ -4,9 +4,15 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { DashboardTaskComponent } from './components/dashboard-task/dashboard-task.component';
 import { NewTaskComponent } from './components/modals/new-task/new-task.component';
+import { DeleteTaskComponent } from './components/modals/delete-task/delete-task.component';
 
 @NgModule({
-  declarations: [DashboardComponent, DashboardTaskComponent, NewTaskComponent],
+  declarations: [
+    DashboardComponent,
+    DashboardTaskComponent,
+    NewTaskComponent,
+    DeleteTaskComponent
+  ],
   imports: [CommonModule, SharedModule]
 })
 export class DashboardModule {}
