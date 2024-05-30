ba79e80613272e4503eedaaec2fc1fc9adf831b1
Rhamanda Lima <cathyannarhamanda@gmail.com>
2024-05-29 21:21:50 -0300

    feat: create dashboard task component

---
+++ task-manager-front-end/src/app/modules/dashboard/components/dashboard-task/dashboard-task.component.ts
@@ -0,0 +1,68 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Task } from 'src/app/shared/interfaces/task.interface';
+import { PriorityColors, PriorityLevel } from '../../enums/priority-level';
+
+@Component({
+  selector: 'app-dashboard-task',
+  templateUrl: './dashboard-task.component.html',
+  styleUrls: ['./dashboard-task.component.scss']
+})
+export class DashboardTaskComponent {
+  @Input() task!: Task;
+  @Output() reloadTasks: EventEmitter<boolean> = new EventEmitter();
+
+  editForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.buildForm();
+    this.verifyColorChange();
+    this.mapValueChanged();
+  }
+
+  private buildForm(): void {
+    this.editForm = this.fb.group({
+      id: [this.task.id],
+      nome: [this.task.nome],
+      descricao: [this.task.descricao],
+      prioridade: [this.task.prioridade],
+      cor: [this.task.cor]
+    });
+  }
+
+  private verifyColorChange(): void {
+    this.editForm.controls['prioridade'].valueChanges.subscribe((value) => {
+      this.setDividerColor(value);
+    });
+  }
+
+  private mapValueChanged(): void {
+    for (const field in this.editForm.controls) {
+      this.editForm.controls[field].valueChanges.subscribe((value) => {
+        if (field !== 'cor') {
+          // TODO: adicionar método de atualização da task
+        }
+        return;
+      });
+    }
+  }
+
+  private setDividerColor(priority: string): void {
+    switch (priority) {
+      case PriorityLevel.HIGH_PRIORITY:
+        this.editForm.controls['cor'].setValue(PriorityColors.HIGH);
+        break;
+      case PriorityLevel.MEDIUM_PRIORITY:
+        this.editForm.controls['cor'].setValue(PriorityColors.MEDIUM);
+        break;
+      case PriorityLevel.LOW_PRIORITY:
+        this.editForm.controls['cor'].setValue(PriorityColors.LOW);
+        break;
+      default:
+        this.editForm.controls['cor'].setValue(PriorityColors.DEFAULT);
+        break;
+    }
+  }
+}
--- task-manager-front-end/src/app/modules/dashboard/dashboard.module.ts
+++ task-manager-front-end/src/app/modules/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { DashboardTaskComponent } from './components/dashboard-task/dashboard-task.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, DashboardTaskComponent],
   imports: [CommonModule, SharedModule]
 })
 export class DashboardModule {}
